347790a5a9bbb12be663c87fa1673a26fe557a43
Angel Araya <angel.araya@janeirodigital.com>
2022-02-03 10:20:23 -0600

    Show webId when logged in/hide when not

---
+++ src/app/views/navbar/navbar.stories.ts
@@ -0,0 +1,24 @@
+
+import { Meta, Story, moduleMetadata } from '@storybook/angular';
+import { NavbarComponent } from "./navbar.component";
+import {DemoMaterialModule} from "../../../material-module";
+
+export default {
+  title: 'Navbar',
+  component: NavbarComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [DemoMaterialModule],
+    })
+  ]
+};
+
+const Template: Story = (args) => ({props: args});
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  webId: 'https://solid.provider/user/profile/card#me',
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
