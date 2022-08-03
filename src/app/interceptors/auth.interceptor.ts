b8e91a16454944081b0424a52b2127dd54c9d03a
Maciej Samoraj <maciej.samoraj@gmail.com>
2022-08-03 18:08:56 +0200

    improving auth interceptor
    Co-authored-by: elf Pavlik <elf-pavlik@hackers4peace.net>

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -4,38 +4,32 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpResponse
+  HttpResponse,
+  HttpEventType
 } from '@angular/common/http';
 import { Observable, tap} from 'rxjs';
-import { Router } from "@angular/router";
+import { CoreActions } from '../actions';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   constructor(
-    private router: Router,
+    private store: Store
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       tap((event: HttpEvent<any>) => {
         // Ignore the outgoing requests
-        if (event.type === 0) return;
+        if (event.type === HttpEventType.Sent) return;
 
         event = event as HttpResponse<unknown>;
         console.log(event.url);
 
         if (event instanceof HttpResponse && event.status === 401) {
-
-          // If we're already in the login page then do not redirect again to login
-          if (window.location.pathname === '/login') {
-            console.log('[Auth Interceptor] Already in login page, not redirecting')
-            return;
-          }
-
-          // TODO (angel) better logging
-          console.log('[Auth Interceptor] Received 401 response - navigating to /login')
-          this.router.navigate(['/login']);
+          console.log('[Auth Interceptor] Received 401 response - setting login status to false')
+          this.store.dispatch(CoreActions.loginStatusChanged({loggedIn: false}));
         }
       })
     );
