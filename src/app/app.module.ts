c9cb68f99265c6d779e0e6111abae8f06f4727e8
elf Pavlik <elf-pavlik@hackers4peace.net>
2022-09-23 18:09:09 -0500

    only enable service worker in production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,7 +66,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: ENV.production }),
     EffectsModule.forRoot(Effects),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: true,
+      enabled: ENV.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
